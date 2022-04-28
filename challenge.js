afccd7cf327f136f74fd26fb0a527056ea3ff223
Kelvin Bush <kelybush@gmail.com>
2022-04-28 22:14:55 +0000

    complete

--- challenge.js
+++ challenge.js
@@ -1,5 +1,36 @@
 function balancedBrackets(string) {
-  // your code here
+ // ignore non brackets
+  string = string.replace(/[^\[\]\{\}\(\)]/g, '');
+  // check if string is empty
+  if (string.length === 0) {
+    return true;
+  }
+  // check if string is balanced
+  if (string.length % 2 !== 0) {
+    return false;
+  }
+  // check if string is balanced
+  let stack = [];
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '(' || string[i] === '[' || string[i] === '{') {
+      stack.push(string[i]);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let last = stack.pop();
+      if (last === '(' && string[i] !== ')') {
+        return false;
+      }
+      if (last === '[' && string[i] !== ']') {
+        return false;
+      }
+      if (last === '{' && string[i] !== '}') {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
 
 console.log(balancedBrackets('(hello)[world]'))
